338ed80436ab8015acc8f066df3a76cd642bd57c
Tristan PONESSA <trponess@e1r4p6.42.fr>
2020-08-27 18:07:01 +0200

    good adv

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 
 #define NB_ALLOCS 100
 
+t_box *l; //global
 
 void *mmap(void *addr, size_t length, int prot, int flags,
            int fd, off_t offset);
@@ -34,50 +35,39 @@ typedef struct s_box {
 
 //************************************************************************************
 
-/*
-void *ft_mmap() {
 
-    void *zone;
 
-    zone = (t_box *)mmap(madr, len, )
-    if (zone == NULL)
-        return NULL;
-}
-*/
 
-t_box *ft_create_elem(void *madr, size_t len) {
+
+t_box *ft_create_elem() {
 
     t_box *elem;
 
-    elem = (t_box *)mmap(madr, sizeof(t_box), PROT_READ | PROT_EXEC | PROT_WRITE, MAP_SHARED, 3 ,0);
-
-    elem->madr = data;
-
-
-
-
-    elem->size = len;
-    elem->available = 1;
-    elem->next = NULL;
-
+    elem = (t_box *)ft_mmap(madr, sizeof(t_box), PROT_READ | PROT_EXEC | PROT_WRITE, MAP_SHARED, 3 ,0);
+    if elem {
+        elem->madr = NULL;
+        elem->size = -1;
+        elem->available = -1;
+        elem->next = NULL;
+    }
     return elem;
 }
 
-t_box *ft_box_last(t_box *blst) {
+void ft_box_push_back(size_t len) {
 
-    while (blst->next != NULL)
-        blst = blst->next;
-    return blst;
-}
+    t_box *blst;
+    t_box *head;
 
-void ft_box_push_back(t_box **begin_lst, void *data) {
-
-    t_box *blst = *begin_lst;
-    t_box *head = blst;
+    if !l{
+        l = ft_create_elem();
+        return ;
+    }
+    blst = l;
+    head = l;
     while (blst->next != NULL)
         blst = blst->next;
     blst->next = ft_create_elem(data);
-    *begin_lst = head;
+    l = head;
 }
 
 void ft_clear_lst(t_box **l) {
@@ -95,53 +85,111 @@ void ft_clear_lst(t_box **l) {
     *l = NULL;
 }
 
-void print_lst(t_box *l) {
-
-    void *data;
+void show_alloc_ex_mem() {
+    
     if (!l)
         return ;
     while (1) {
-        data = l->data;
-        printf("data %s\n", (char *)data);
+        printf("adr %p ", l->madr);
+        printf("size %z\n", l->len);
         if (l->next == NULL)
             break;
         l = l->next;
     }
 }
 
+void fatal(char *msg) {
 
-void show_alloc_ex_mem();
+    ft_put_fd(STDERR, msg);
+    //free all
+    exit(0);
+}
+
+void *ft_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
+
+    void *zone;
+
+    zone = (t_box *)mmap(addr, length, prot, flags, fd, offset);
+    if (zone == MAP_FAILED)//(void *) -1
+        return NULL;
+    return zone;
+}
 
 void free(void *ptr)
 {
+    int r;
+
+    if !ptr
+        return ;
     t_box *ibox = (t_box*)ptr;
-    munmap(ibox->ptr, ibox->len);
+    r = munmap(ibox->ptr, ibox->len);
+    if (r == -1)
+        fatal("munmap failed with -1");
 }
+
 
 void *malloc(size_t size)
 {
-    t_box *ibox;
 
-    if (size == 0)
-        return NULL;
-    if (size > TINY_MIN && size <= SMALL_MAX)
-    {
+    t_box *i;
+    int res = 0;
 
+    i = lst;
+    while (1) {
+
+
+        //didnt find anything in tiny or small
+        if (!blst || blst->next == NULL)
+            res = add_small_tiny(&lst);
+            if (res == -1)
+
+            return i;
+        if (len >= LARGE_MIN)
+            //create zone
+            res = ft_box_push_back(&lst, len);
+            //go to end of list
+            if (res == -1)
+                return NULL;
+            return i;//go to end of list change i
+        if (len >= TINY_MIN && len <= TINY_MAX && i->madr) || (len >= TINY_MIN && len <= TINY_MAX && i->madr)
+            res = 1;
+
+        if res == -1
+            return NULL;
+        if res == 1
+            return i;
+
+        i = i->next;
     }
-    //if (size >= SMALL_MIN && size <= SMALL_MAX)
-    //{
+}
 
-    //}
-    if (size >= TINY_MIN)
-    {
-
-    }
-
-
-
+int add_bunch() {
 
 }
 
+t_box *get_available_tbox(t_size len) {
+
+}
+//adds 100 tiny small to cur lst
+int add_small_tiny(t_box **l) {
+
+    int i = 0;
+    int res = 0;
+    //t_box* l = ft_create_elem(&i);
+    while i < NB_ALLOCS * 2 {
+        if (i <= NB_ALLOCS)
+            res = ft_list_push_back(&l, TINY);
+        else
+            res = ft_list_push_back(&l, SMALL);
+        ++i;
+    }
+    return res;
+}
+
+void add_large(t_box **l ,size_t len) {
+
+    ft_list_push_back(&l, len);
+}
 
 void *realloc(void *ptr, size_t size);
 //
@@ -150,11 +198,7 @@ void *realloc(void *ptr, size_t size);
 
 int main() {
 
-    t_box *tiny_allocs;
-    t_box *small_allocs;
-    t_box *large_allocs;
 
-    t_box *allocs;
 
 
 }
