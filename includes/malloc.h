ba71b81f393075d945e450a96a1d86c4dea990a8
Tristan PONESSA <trponess@e2r11p8.42.fr>
2021-02-26 16:18:36 +0100

    added note

--- includes/malloc.h
+++ includes/malloc.h
@@ -17,7 +17,26 @@
 #define LARGE_MIN 33
 #define NB_ALLOCS 100
 
+#define TINY_HEAP_ALLOCATION_SIZE (4 * getpagesize())
+#define TINY_BLOCK_SIZE (TINY_HEAP_ALLOCATION_SIZE / 128)
+#define SMALL_HEAP_ALLOCATION_SIZE (16 * getpagesize())
+#define SMALL_BLOCK_SIZE (SMALL_HEAP_ALLOCATION_SIZE / 128)
 
+typedef struct s_heap {
+    struct s_heap	*prev;
+    struct s_heap	*next;
+    t_heap_group	group;
+    size_t		total_size;
+    size_t		free_size;
+    size_t		block_count;
+} t_heap;
+
+typedef struct s_block {
+    struct s_block	*prev;
+    struct s_block	*next;
+    size_t		data_size;
+    bool		freed;
+} t_block;
 
 
 #endif //MALLOC_MALLOC_H
